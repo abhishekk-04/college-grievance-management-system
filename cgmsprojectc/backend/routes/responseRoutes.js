d4a13e61564f46d5fe83e12846694dcccbdc08a9
itz-me-abhishek28 <abhishekgautam02652@gmail.com>
2026-05-31 15:44:32 +0530

    Remove duplicate project folder

--- cgmsprojectc/backend/routes/responseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addResponse, getResponsesForGrievance } = require('../controllers/responseController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, addResponse);
-router.get('/:grievanceId', protect, getResponsesForGrievance);
-
-module.exports = router;
